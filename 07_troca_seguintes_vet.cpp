1e47a20d00b8c9dc7fb4a277a41d47b6d11f4f02
Elton Viana <eltonvs@outlook.com>
2016-02-02 18:41:31 -0300

    Added troca_seguintes.cpp

---
+++ 07_troca_seguintes_vet.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	int b[20];
+
+	// Read vector values
+	for (int i = 0; i < 20; i++)
+		cin >> b[i];
+
+	// Print vector
+	for (int i = 0; i < 20; i++)
+		cout << "vet[" << i << "] = " << b[i] << endl;
+
+	cout << "Changing positions (odd <-> even)...";
+
+	// Change positions (odd <-> even)
+	for (int i = 0, t; i < 20 - 1; i += 2)
+		t = b[i], b[i] = b[i+1], b[i+1] = t;
+
+	// Print new vector
+	for (int i = 0; i < 20; i++)
+		cout << "vet[" << i << "] = " << b[i] << endl;
+
+	return EXIT_SUCCESS;
+}
