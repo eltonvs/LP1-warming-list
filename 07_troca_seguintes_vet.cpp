3445eb6dbafec4ff81faf7fa5dd6cb062ff7a663
Elton Viana <eltonvs@outlook.com>
2016-02-03 17:50:54 -0300

    Fix question interpretation

--- 07_troca_seguintes_vet.cpp
+++ 07_troca_seguintes_vet.cpp
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[]) {
 	cout << "Changing positions (odd <-> even)..." << endl;
 
 	// Change positions (odd <-> even)
-	for (int i = 0, t; i < 20 - 1; i += 2)
+	for (int i = 1, t; i < 20 - 1; i += 2)
 		t = b[i], b[i] = b[i+1], b[i+1] = t;
 
 	// Print new vector
