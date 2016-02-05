2377dfdbcf8ed16c105bc457fd751722de7efdca
Elton Viana <eltonvs@outlook.com>
2016-02-05 20:51:11 -0300

    Fix graphical accents on problem description

--- 12_hanoi.cpp
+++ 12_hanoi.cpp
@@ -2,11 +2,11 @@
  * Question:
  * Implemente um programa em C++ chamado hanoi.cpp que resolve, recursivamente,
  * o problema das Torres de Hanoi. O problema das Torres de Hanoi consiste em 3
- * pinos - A (origem), B (destino) e C (auxiliar) - e n discos de diãmetros diferentes.
+ * pinos - A (origem), B (destino) e C (auxiliar) - e n discos de diâmetros diferentes.
  * Inicialmente, todos os discos se encontram empilhados no pino origem (A), em ordem
  * decrescente de tamanho, de baixo para cima. O objetivo é empilhar todos os discos
  * no pino-destino (B), atendendo às seguintes restrições: (i) apenas um disco pode ser
- * movido de cada vez, e; (ii) qualquer disco n ̃ao pode ser jamais colocado sobre outro de
+ * movido de cada vez, e; (ii) qualquer disco não pode ser jamais colocado sobre outro de
  * tamanho menor.
  * O programa deve receber, via linha de comando, um valor inteiro positivo correspondente
  * ao número de discos a ser resolvido e deve exibir a sequẽncia de movimentos para achar a
