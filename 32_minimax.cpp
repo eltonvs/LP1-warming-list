319bdd30a3e75bef77d474a7a6366952b2a2877b
Elton Viana <eltonvs@outlook.com>
2016-02-10 00:06:01 -0300

    Added minimax

---
+++ 32_minimax.cpp
@@ -0,0 +1,43 @@
+/*
+ * Question:
+ * Na teoria dos Sistemas define-se como elemento minimáx de uma matriz, o menor
+ * elemento da linha em que se encontra o maior elemento da matriz. Implemente um
+ * programa em C++ chamado minmax.cpp que lê uma matriz A[10][10] e determina
+ * o elemento minimáx desta matriz, imprimindo a matriz A e a posição do elemento
+ * minimáx.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	int *a, b, b_row;
+
+	// Allocate memory to a
+	a = new int [10*10];
+
+	// Get matrix content
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++) {
+			cout << "A[" << i << "][" << j << "] = ";
+			cin >> *(a+i*10+j);
+			if (*(a+i*10+j) > b || (i == 0 && j == 0)) b = *(a+i*10+j), b_row = i;
+		}
+
+	// Create minimax var
+	int minimax[] = {*(a+b_row*10), b_row, 0};
+
+	// Get minimax
+	for (int i = 0; i < 10; i++)
+		if (*(a+b_row*10+i) < minimax[0]) minimax[0] = *(a+b_row*10+i), minimax[2] = i;
+
+	// Print matrix
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			cout << (j == 0 ? "| " : " ") << *(a+i*10+j) << (j == 9 ? " |\n" : " ");
+
+	cout << "The minimax element is A[" << minimax[1] << "][" << minimax[2] << "]  = " << minimax[0] << endl;
+
+	return EXIT_SUCCESS;
+}
