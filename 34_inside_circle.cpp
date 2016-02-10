b3e7bfc971a10c52b5b3c34e1f9f8fee90361f15
Elton Viana <eltonvs@outlook.com>
2016-02-10 01:00:33 -0300

    Added inside_circle

---
+++ 34_inside_circle.cpp
@@ -0,0 +1,54 @@
+/*
+ * Question:
+ * Considerando a estrutura da questão 29 para representar um ponto em uma grade 2D,
+ * implemente uma função que indique se um ponto p está localizado dentro ou fora de
+ * um círculo. O círculo é definido por seu centro c e seu raio r. A função deve retornar 1
+ * caso o ponto esteja localizado dentro ou na borda do círculo e 0 (zero) caso contrário.
+ * Essa função deve obedecer o protótipo:
+ *   int dentroCirc (const Ponto *c, int raio, const Ponto *p);
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+typedef struct _Dot {
+	int x;
+	int y;
+} Dot;
+
+int inside_circle(const Dot *c, int r, const Dot *p);
+double distance_between_dots(const Dot *a, const Dot *b);
+
+int main(int argc, char const *argv[]) {
+	Dot c, p;
+	int r;
+
+	// Get variables
+	cout << "Enter the coordinates from the center of circle:" << endl;
+	cout << "  X = ";
+	cin >> c.x;
+	cout << "  Y = ";
+	cin >> c.y;
+	cout << "Enter the circle radius: ";
+	cin >> r;
+	cout << "Enter the dot coordinates:" << endl;
+	cout << "  X = ";
+	cin >> p.x;
+	cout << "  Y = ";
+	cin >> p.y;
+
+	// Output
+	cout << "The dot " << (inside_circle(&c, r, &p) ? "is" : "isn't") << " inside the circle"  << endl;
+
+	return EXIT_SUCCESS;
+}
+
+double distance_between_dots(const Dot *a, const Dot *b) {
+	return sqrt((double)pow((*a).x - (*b).x, 2) + pow((*a).y - (*b).y, 2.0));
+}
+
+int inside_circle(const Dot *c, int r, const Dot *p) {
+	return distance_between_dots(c, p) <= r ? 1 : 0;
+}
